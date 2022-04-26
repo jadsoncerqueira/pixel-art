9a3f4386bc5245e72e6301b34d3f5b8673ce8e40
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-04-25 22:06:07 -0300

    ajustando tamnho de pixels para 40 e adicionando borda de 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-geradorQuadroPixel(5,5)
+geradorQuadroPixel(5,5,40)
 
-function geradorQuadroPixel (linhas, colunas) {
+function geradorQuadroPixel (linhas, colunas, tamanho) {
   for (let i = 0; i < colunas; i += 1) {
 
     let divPai = document.querySelector('#pixel-board')
@@ -10,8 +10,12 @@ function geradorQuadroPixel (linhas, colunas) {
     for (let i2 = 0; i2 < linhas; i2 += 1) {
         let pixels = document.createElement('div');
         pixels.className = 'pixel'
+        pixels.style.width = tamanho + 'px'
+        pixels.style.height = tamanho + 'px'
+        pixels.style.backgroundColor = 'white'
         colums.appendChild(pixels)
     }
+    divPai.style.width =  ((tamanho + 2)  * linhas) + 'px'
     divPai.appendChild(colums)
   }
 }
