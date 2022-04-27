1dc25f0e984eca9623214ac73bcc328ea825530d
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-04-27 18:42:46 -0300

    organizando codigo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function (){
+window.onload = function inicial(){
   let corPreta = document.querySelector('.color');
   corPreta.className = 'color preto selected';
   localStorage.setItem('cor', 'black');
@@ -11,21 +11,30 @@ for(let i3 = 0; i3 < elementoSelecionado.length; i3 += 1) {
   elementoSelecionado[i3].addEventListener('click', selecao);
 }
 
-function geradorQuadroPixel (linhas, colunas, tamanho) {
+function colorPixel(event) {
+  const pixel = event.target;
+  if (pixel.style.backgroundColor === 'white') {
+    pixel.style.backgroundColor = localStorage.getItem('cor');
+  } else {
+    pixel.style.backgroundColor = 'white';
+  }
+}
+
+function geradorQuadroPixel(linhas, colunas, tamanho) {
   for (let i = 0; i < colunas; i += 1) {
     let divPai = document.querySelector('#pixel-board');
-    let colums = document.createElement('div');
+    const colums = document.createElement('div');
     colums.className = 'pixel-coluna';
     for (let i2 = 0; i2 < linhas; i2 += 1) {
-      let pixels = document.createElement('div');
+      const pixels = document.createElement('div');
       pixels.className = 'pixel';
-      pixels.style.width = tamanho + 'px';
-      pixels.style.height = tamanho + 'px';
+      pixels.style.width = String(tamanho + 'px');
+      pixels.style.height = String(tamanho + 'px');
       pixels.style.backgroundColor = 'white';
       pixels.addEventListener("click", colorPixel);
       colums.appendChild(pixels);
     }
-    divPai.style.width = ((tamanho + 2) * linhas) + 'px';
+    divPai.style.width = String(((tamanho + 2) * linhas) + 'px');
     divPai.appendChild(colums);
   }
 }
@@ -34,19 +43,10 @@ function selecao(pix) {
   const pixelColor = document.getElementsByClassName('color');
   for (let i = 0; i < pixelColor.length; i += 1) {
     if (pixelColor[i].className.includes('selected')) {
-      let elemento = pix.target;
+      const elemento = pix.target;
       pixelColor[i].className = pixelColor[i].className.replace(' selected', '');
-      elemento.className = elemento.className + ' selected';
+      elemento.className = String(elemento.className + ' selected');
       localStorage.setItem('cor', elemento.style.backgroundColor);
     }
-  }
-}
-
-function colorPixel(event) {
-  const pixel = event.target;
-  if (pixel.style.backgroundColor === 'white') {
-    pixel.style.backgroundColor = localStorage.getItem('cor');
-  } else {
-    pixel.style.backgroundColor = 'white';
   }
 }
